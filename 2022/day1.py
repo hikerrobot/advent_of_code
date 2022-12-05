a938e4b668ce3c945ab9709b3647caef3383af82
robert96028 <robert96028@gchq.gov.uk>
2022-12-05 09:40:54 +0000

    added missing calc for day 1 pt2

--- 2022/day1.py
+++ 2022/day1.py
@@ -5,7 +5,7 @@ sys.path.append('../')
 import read_file as rf
 
 fileContents = rf.get_file_contents('input-d1')
-print("file lines = {}".format(len(fileContents)))
+# print("file lines = {}".format(len(fileContents)))
 
 elf = 1
 elf_calories_list = []
@@ -15,7 +15,7 @@ for line in fileContents:
 
     # print(line)
     if line == "":
-        print("storing elf {} with calories {}".format(elf, calories))
+        # print("storing elf {} with calories {}".format(elf, calories))
         elf_cal_dict[elf] = calories
         elf += 1
         calories = 0
@@ -24,5 +24,16 @@ for line in fileContents:
     calories += int(line)
 
 # sort dict by value
-sorted_dict = sorted(elf_cal_dict.items(), key=lambda item: item[1])
-print(sorted_dict)
+# could've been better by using other than dict, as didn't need elf count as key
+sorted_cal_list = sorted(elf_cal_dict.items(), key=lambda item: item[1])
+
+# print(type(calories_list))
+# print(calories_list[len(calories_list)-2:])
+
+numElvesCals = 2
+total_cals = 0
+for i in range(len(sorted_cal_list)-numElvesCals, len(sorted_cal_list)):
+    print(sorted_cal_list[i][1])
+    total_cals += sorted_cal_list[i][1]
+
+print("total cals {} for {} elves".format(total_cals, numElvesCals))
