7f5803337a85d35500025b49dc39abe767a7647b
hikerrobot <hikerrobot@gmail.com>
2022-12-11 16:35:06 +0000

    using logging module added input file for d2, covering all scenarios. fixed bug causing incorrect d2 answer

--- 2022/day2-2.py
+++ 2022/day2-2.py
@@ -1,11 +1,14 @@
-import sys
+import sys, logging
 
 sys.path.append('../')
 
 import read_file as rf
 
+# logger = logging.getLogger('aoclogger')
+logging.basicConfig(level=logging.INFO)
+# logger.setLevel(logging.INFO)
 file_content = rf.get_file_contents('input-d2')
-# print("file lines = {}".format(len(fileContents)))
+
 
 
 # each line in the file is a round
@@ -25,6 +28,8 @@ file_content = rf.get_file_contents('input-d2')
 
 #foo_input = ['C X', 'A Y', 'A Z', 'B Y', 'C Y']
 
+
+
 elf_opponent_rps = {
     'A':'rock','B':'paper','C':'scissors'
 }
@@ -42,33 +47,32 @@ game_win_rules = {
 
 
 game_lose_rules = {
-    'rock':'paper',
+    'rock':'scissors',
     'paper':'rock',
-    'scissors':'rock'
+    'scissors':'paper'
 }
 
 rps_points = {
     'rock':1,'paper':2,'scissors':3
 }
 
-# TODO need to work out the correct piece to match the desired result
-# X means lose
-# Y means draw
-# Z means win
+outcome = {
+    'X':'lose','Y':'draw','Z':'win'
+}
 
-def round_result_via_known_outcome(opponent_choice, known_outcome):
-    print("oc {} outcome {}".format(opponent_choice, known_outcome))
-    if known_outcome == 'X': #lose
+def round_result_via_known_outcome(opponent_choice, round_outcome):
+    logging.debug("opponent choice: {}, round outcome: {}".format(opponent_choice, outcome[round_outcome]))
+    if round_outcome == 'X': #lose
         my_choice = game_lose_rules[opponent_choice]
-        print("lose, my choice {}".format(my_choice))
+        logging.debug("lose, my choice {}".format(my_choice))
         return rps_points[my_choice]
-    elif known_outcome == 'Y': #draw
+    elif round_outcome == 'Y': #draw
         my_choice = opponent_choice
-        print("draw, my choice {}".format(my_choice))
+        logging.debug("draw, my choice {}".format(my_choice))
         return 3 + rps_points[my_choice]
     else: # win 'Z'. could add check here to error if not known.
         my_choice = game_win_rules[opponent_choice]
-        print("win, my choice {}".format(my_choice))
+        logging.debug("win, my choice {}".format(my_choice))
         return 6 + rps_points[my_choice]
 
 def determine_who_won(decoded_round):
@@ -89,23 +93,20 @@ def decode_round(round):
 def process_rounds(game_rounds):
 
     total_score = 0
-    print("game rounds = {}".format(len(game_rounds)))
+    logging.info("game rounds = {}".format(len(game_rounds)))
 
     for i in range(len(game_rounds)):
         round = game_rounds[i].split(' ')
 
         decoded_round = decode_round(round)
-        print("decoded_round {}".format(decoded_round))
+        logging.debug("decoded_round {}".format(decoded_round))
         round_result = determine_who_won(decoded_round)
-        print("round result {}".format(round_result))
+        logging.debug("round result {}".format(round_result))
         total_score += round_result
-        print("=======================================")
+        logging.debug("=======================================")
     
-    print("total score {}".format(total_score))
+    logging.info("total score {}".format(total_score))
 
 process_rounds(file_content)
 
 
-
-
-# def calc_score(round):
--- read_file.py
+++ read_file.py
@@ -1,5 +1,8 @@
+import logging
+
 def get_file_contents(filename):
-    print("reading file {}".format(filename))
+    #  TODO get logger working. too tired... yawn
+    logging.info("reading file {}".format(filename))
     with open(filename) as f:
         lines = f.read().splitlines()
 
